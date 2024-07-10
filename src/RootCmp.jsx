69f760fe1f9a4863dc370274aba64703babe8c74
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2024-07-10 12:31:12 +0300

    Init repo

---
+++ src/RootCmp.jsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <section className="app">
+      <main></main>
+    </section>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom/client'
+import { App } from './RootCmp.jsx'
+import './assets/style/main.scss'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
