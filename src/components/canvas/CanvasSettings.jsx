14efe4bce9bbb8b60f4c203a0d1cfdc057e0b522
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2024-07-13 22:56:04 +0300

    Show numbers in canvas settings

--- src/components/canvas/CanvasSettings.jsx
+++ src/components/canvas/CanvasSettings.jsx
@@ -1,7 +1,6 @@
 import { memo, useEffect, useRef, useState } from 'react'
 
 import { canvasService } from '../../services/canvas.service'
-import { hideTooltip, showTooltip } from '../../services/event-bus.service'
 import { utilService } from '../../services/util.service'
 
 import { useTheme } from '../../customHooks/useTheme'
@@ -37,13 +36,11 @@ function CanvasSettingsCmp({
     let { value, name: field } = target
     settingsRef.current = { ...settingsRef.current, [field]: +value }
     setForcedRendered(prev => prev + 1)
-    showTooltip(target, value)
   }
 
   function onSetPenWidth({ target }) {
     const { value } = target
     setPen(prevPen => ({ ...prevPen, width: +value }))
-    showTooltip(target, value)
   }
 
   function onResetSettings() {
@@ -66,44 +63,50 @@ function CanvasSettingsCmp({
       <form className="settings-form flex column">
         <div className="input-container flex align-center justify-between">
           <label htmlFor="penWidth">Pen width</label>
-          <input
-            type="range"
-            name="penWidth"
-            onChange={onSetPenWidth}
-            value={penWidth}
-            min={1}
-            max={30}
-            onMouseEnter={ev => showTooltip(ev.currentTarget, penWidth)}
-            onMouseLeave={hideTooltip}
-          />
+
+          <div className="container">
+            <input
+              type="range"
+              name="penWidth"
+              onChange={onSetPenWidth}
+              value={penWidth}
+              min={1}
+              max={30}
+            />
+            <span>{penWidth}</span>
+          </div>
         </div>
 
         <div className="input-container flex align-center justify-between">
           <label htmlFor="sizeDiff">Resize by</label>
-          <input
-            type="range"
-            name="sizeDiff"
-            onChange={onUpdateSettings}
-            value={sizeDiff}
-            min={1}
-            max={20}
-            onMouseEnter={ev => showTooltip(ev.currentTarget, sizeDiff)}
-            onMouseLeave={hideTooltip}
-          />
+
+          <div className="container">
+            <input
+              type="range"
+              name="sizeDiff"
+              onChange={onUpdateSettings}
+              value={sizeDiff}
+              min={1}
+              max={20}
+            />
+            <span>{sizeDiff}</span>
+          </div>
         </div>
 
         <div className="input-container flex align-center justify-between">
           <label htmlFor="rotateAngle">Rotation angle</label>
-          <input
-            type="range"
-            name="rotateAngle"
-            onChange={onUpdateSettings}
-            value={rotateAngle}
-            min={1}
-            max={360}
-            onMouseEnter={ev => showTooltip(ev.currentTarget, rotateAngle)}
-            onMouseLeave={hideTooltip}
-          />
+
+          <div className="container">
+            <input
+              type="range"
+              name="rotateAngle"
+              onChange={onUpdateSettings}
+              value={rotateAngle}
+              min={1}
+              max={360}
+            />
+            <span>{rotateAngle}</span>
+          </div>
         </div>
 
         <div className="options-container flex justify-between">
